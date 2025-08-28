d11fc6cc47fee58cd101e4f005e9a122922e8de7
leotesler <leotesler2026@u.northwestern.edu>
2025-08-28 11:57:22 -0400

    Data update 8/28

--- ex.R
+++ ex.R
@@ -4,6 +4,7 @@
 library(tidyverse)
 library(baseballr)
 library(rvest)
+library(httr)
 library(jsonlite)
 
 # get data ----
@@ -11,6 +12,16 @@ years <- c(2004:2025)
 
 transactions <- map_df(years, function(x) {mlb_people_free_agents(x)})
 
+# pull spotrac data ----
+url <- "https://www.spotrac.com/mlb/new-york-yankees/yearly/_/sort/cap_total2/view/roster"
+
+response <- GET(url)
+
+html <- read_html(content(response, "text", encoding = "UTF-8"))
+
+tables <- html_table(html, fill = TRUE)
+
+# explore ----
 predictions |> 
   group_by(pitcher_name, pitch_type) |> 
   summarize(pitch_grade = mean(pitch_grade, na.rm = TRUE),
