6d547d05e23b93727d93ccdf1e5996c682b99e2c
leotesler <leotesler2026@u.northwestern.edu>
2025-08-31 17:21:59 -0400

    Data update 8/31

--- ex.R
+++ ex.R
@@ -6,21 +6,46 @@ library(baseballr)
 library(rvest)
 library(httr)
 library(jsonlite)
-
-# get data ----
-years <- c(2004:2025)
-
-transactions <- map_df(years, function(x) {mlb_people_free_agents(x)})
+library(chromote)
 
 # pull spotrac data ----
-url <- "https://www.spotrac.com/mlb/new-york-yankees/yearly/_/sort/cap_total2/view/roster"
+teams <- c("arizona-diamondbacks", "athletics", "atlanta-braves",
+           "baltimore-orioles", "boston-red-sox", "chicago-cubs",
+           "chicago-white-sox", "cincinnati-reds", "cleveland-guardians",
+           "colorado-rockies", "detroit-tigers", "houston-astros",
+           "kansas-city-royals", "los-angeles-angels", "los-angeles-dodgers",
+           "miami-marlins", "milwaukee-brewers", "minnesota-twins",
+           "new-york-mets", "new-york-yankees", "philadelphia-phillies",
+           "pittsburgh-pirates", "san-diego-padres", "san-francisco-giants",
+           "seattle-mariners", "st-louis-cardinals", "tampa-bay-rays",
+           "texas-rangers", "toronto-blue-jays", "washington-nationals")
 
-response <- GET(url, add_headers(`User-Agent` = "Mozilla/5.0"))
+contract_data <- list()
 
-page <- read_html(content(response, "text", encoding = "UTF-8"))
+b <- ChromoteSession$new()
 
-rows <- page |> 
-  html_elements(".team-roster__row")
+for (team_name in teams) {
+  url <- paste0("https://www.spotrac.com/mlb/", team_name, "/yearly/_/sort/cap_total2/view/roster")
+  
+  message("Loading ", team_name)
+  b$Page$navigate(url)
+  Sys.sleep(5)
+  
+  html <- b$DOM$getDocument()
+  node <- b$DOM$getOuterHTML(nodeId = html$root$nodeId)
+  page <- read_html(node$outerHTML)
+  
+  tables <- page |> 
+    html_elements("table") |> 
+    html_table(fill = TRUE)
+  
+  contract_status <- tables[[2]] |> 
+    janitor::clean_names()
+  
+  contract_data[[team_name]] <- contract_status
+}
+
+contract_data <- bind_rows(contract_data)
 
 # explore ----
 predictions |> 
